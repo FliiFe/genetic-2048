eb150ae9ea306fb582039965f4650616a3946418
FliiFe <theophile.cailliau@gmail.com>
2016-06-20 10:16:16 +0200

    Cleaned the script up.

--- src/application.js
+++ src/application.js
@@ -31,7 +31,7 @@ function getBestTileFromGrid(grid) {
     return Math.max.apply(this, grid);
 }
 
-// Movement from 0 to 3 (inclusive). Will be floored.
+// Array. Biggest number's index defines the movement.
 // 0 = up, 2 = down
 // 1 = right, 3 = left
 function move(mv) {
@@ -45,67 +45,11 @@ function tryAgain() {
 }
 
 var delay = 4;
-var popSize = 10;
-var networks = [];
-var genetics = undefined;
 
-function initializePopulation() {
-
-    for (var i = 0; i < popSize; i++) {
-        networks.push(new Brainwave.Network(16, 4, 50, 100));
-    }
-
-    genetics = new Brainwave.Genetics(popSize, networks[0].getNumWeights());
-
-    for (var j = 0; j < popSize; j++) {
-        networks[j].importWeights(genetics.population[j].weights);
-    }
-}
-
-function evolve() {
-
-
-    genetics.epoch(genetics.population);
-
-    for (var n = 0; n < popSize; n++) {
-        networks[n].importWeights(genetics.population[n].weights);
-    }
-}
-
-initializePopulation();
-
-var networkIndex = 0;
-var generation = 0;
-var previousThreeGrids = [0, 1, 2];
-var bestTile = 0;
-var bestGenome = undefined;
 // For stat analysis
 var avgs = [];
 
 function run() {
-    if (window.gameManager.isGameTerminated() || (arraysEquality(previousThreeGrids[0], previousThreeGrids[1], previousThreeGrids[2]))) {
-        genetics.population[networkIndex].fitness = window.gameManager.score;
-        bestTile = Math.max(bestTile, getBestTileFromGrid(getGrid()));
-        //bestGenome = genetics.sortGenomes(genetics.population)[0];
-        //console.log('Individual ' + networkIndex + ' of generation ' + generation + ', with fitness ' + window.gameManager.score);
-        networkIndex++;
-        tryAgain();
-    }
-    if (networkIndex >= popSize) {
-        genetics.calcStats();
-        var avg = genetics.averageFitness;
-        avgs.push(avg);
-        showAndSendAverage(avg);
-        evolve();
-        networkIndex = 0;
-        generation++;
-    }
-    var movement = networks[networkIndex].run(getGrid()).map(function (m) {
-        return 4 * m;
-    });
-    move(movement);
-    previousThreeGrids.splice(0, 1);
-    previousThreeGrids.push(getGrid());
     setTimeout(run, delay);
 }
 setTimeout(run, 200);
