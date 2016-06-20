47cbfada44ca343e45dfe9c85e06df04ad0607c0
FliiFe <theophile.cailliau@gmail.com>
2016-06-20 10:21:39 +0200

    Added default example for reinforcejs as a basis.

--- src/application.js
+++ src/application.js
@@ -49,6 +49,20 @@ var delay = 4;
 // For stat analysis
 var avgs = [];
 
+var env = {};
+env.getNumStates = function() { return 8; }
+env.getMaxNumActions = function() { return 4; }
+
+// create the DQN agent
+var spec = { alpha: 0.01 } // see full options on DQN page
+agent = new RL.DQNAgent(env, spec); 
+
+setInterval(function(){ // start the learning loop
+  var action = agent.act(s); // s is an array of length 8
+  var reward = 0; //... execute action in environment and get the reward
+  agent.learn(reward); // the agent improves its Q,policy,model, etc. reward is a float
+}, 0);
+
 function run() {
     setTimeout(run, delay);
 }
